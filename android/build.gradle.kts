a3e61955f954f8695e6a0fc8242555b207b5a6af
yingwuhola <holawuyy@outlook.com>
2025-07-14 16:57:31 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pomodoro_app"
-    compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    namespace = "com.example.pomobot"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.pomodoro_app"
+        applicationId = "com.example.pomobot"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
